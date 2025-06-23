695795c15b8881ab82dec97b7eba3cbb4b4b575d
ferdyodhiambo <oferdinanddev112@gmail.com>
2025-06-23 09:52:09 +0300

    add 'scores' package with scoring functionality based on letter values

---
+++ pool-exercise/scores/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn score(input: &str) -> u64 {
+    input
+        .chars()
+        .filter_map(|c| match c.to_ascii_uppercase() {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => Some(1),
+            'D' | 'G' => Some(2),
+            'B' | 'C' | 'M' | 'P' => Some(3),
+            'F' | 'H' | 'V' | 'W' | 'Y' => Some(4),
+            'K' => Some(5),
+            'J' | 'X' => Some(8),
+            'Q' | 'Z' => Some(10),
+            _ => None,
+        })
+        .sum()
+}
